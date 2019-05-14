2ca4cba6ee8b4d6e9d19bd28c6046b0bae370f55
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-14 15:38:39 +0300

    [FEATURE Fixes #165955620] add onSideMenu function

---
+++ UI/js/dashboard.js
@@ -0,0 +1,7 @@
+/*jshint esversion: 6 */
+function onSideMenu() {
+    const sideMenu = document.querySelector('#side-menu');
+    sideMenu.style.width = '250px';
+    const main = document.querySelector('#main');
+    main.style.marginLeft = '250px';
+}
