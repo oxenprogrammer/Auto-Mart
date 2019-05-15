9d32e06ad704c0bcdb937986c979ae3761d37e9d
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-15 17:21:16 +0300

    [CHORE Delivers #165956277 #165956262] complete mark as sold

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -23,21 +23,39 @@ function offSideMenu() {
 }
 
 const togglePostAd = () => {
+    const purchaseOrderList = document.querySelector('#purchase-order-list');
     const postAd = document.querySelector('#post-ad');
     const postAdList = document.querySelector('#post-ad-list');
     postAd.style.display = 'flex';
     postAdList.style.display = 'none';
+    purchaseOrderList.style.display = 'none';
 };
 
 const togglePostAdList = () => {
     const postAd = document.querySelector('#post-ad');
+    const purchaseOrderList = document.querySelector('#purchase-order-list');
     const postAdList = document.querySelector('#post-ad-list');
     postAd.style.display = 'none';
+    purchaseOrderList.style.display = 'none';
     postAdList.style.display = 'flex';
     postAdList.style.marginLeft = '250px';
 };
 
+const togglePurchaseOrderList = () => {
+    const purchaseOrderList = document.querySelector('#purchase-order-list');
+    const postAd = document.querySelector('#post-ad');
+    const postAdList = document.querySelector('#post-ad-list');
+    purchaseOrderList.style.display = 'flex';
+    postAd.style.display = 'none';
+    postAdList.style.display = 'none';
+    postAdList.style.marginLeft = '250px';
+};
+
 const sold = () => {
+    const soldout1 = document.querySelector('.sold1');
+    const soldout2 = document.querySelector('.sold2');
+    const soldout3 = document.querySelector('.sold3');
+    const soldout4 = document.querySelector('.sold4');
     const soldStatus = document.querySelector('#sold-status');
     const soldLink = document.querySelector('#sold');
     // const content = document.createTextNode('Sold');
@@ -45,5 +63,9 @@ const sold = () => {
     soldLink.innerHTML = 'Sold';
     soldStatus.style.color = 'red';
     soldLink.style.background = 'green';
+    soldout1.style.background = 'green';
+    soldout1.innerHTML = 'SOLD OUT';
+    soldout1.style.color = 'white';
+ 
 
 }
