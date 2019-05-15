d6b33fc873cc9ce2619a04105dd4d99ef79613c9
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-15 10:37:00 +0300

    [FEATURE Fixes #165955620] update margin left on smaller screens

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -1,9 +1,14 @@
 /*jshint esversion: 6 */
 function onSideMenu() {
+    const mobile = window.matchMedia('@media(max-width: 568px)');
     const sideMenu = document.querySelector('#side-menu');
     sideMenu.style.width = '250px';
     const main = document.querySelector('#main');
-    main.style.marginLeft = '250px';
+    if (mobile) {
+        main.style.marginLeft = '250px';
+    } else {
+        main.style.marginLeft = '120px';
+    }
     const navbar = document.querySelector('.navbar');
     navbar.style.marginLeft = '250px';
 }
