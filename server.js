a92024077d610b05b71a320a6ef6931830384d12
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-25 01:16:27 +0300

    ADD: [FEATURE Fixes #166250221] signup user

--- server.js
+++ server.js
@@ -3,10 +3,15 @@
 /* jshint node: true */
 
 import express from 'express';
+import bodyParser from 'body-parser';
 import model from './db/db';
 
 // Set up the express app
 const app = express();
+
+// in-built middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 // get all users
 app.get('/api/v1/user', (req, res) => {
@@ -14,6 +19,61 @@ app.get('/api/v1/user', (req, res) => {
     success: 'true',
     message: 'successfully retrieved users',
     users: model.user
+  });
+});
+
+// register user
+app.post('/api/v1/auth/signup', (req, res) => {
+  let emailExist = '';
+  model.user.map((oneUser) => {
+    emailExist = oneUser.email;
+  });
+  if (req.body.email === emailExist) {
+    return res.status(409).send({
+      status: 409,
+      error: `user with email ${req.body.email} exists`
+    });
+  } else if (
+    !req.body.email ||
+    !req.body.first_name ||
+    !req.body.last_name ||
+    !req.body.password ||
+    !req.body.address ||
+    !req.body.user_class
+  ) {
+    return res.status(400).send({
+      status: 400,
+      error: 'Something went wrong, Internal Server Error'
+    });
+  }
+  let last_userId = 0;
+  if (model.user.length > 0) {
+    last_userId = model.user[model.user.length - 1].id;
+  }
+
+  const newUser = {
+    id: last_userId + 1,
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    address: req.body.address,
+    is_admin: false,
+    user_class: req.body.user_class,
+    password: req.body.password
+  };
+
+  model.user.push(newUser);
+
+  return res.status(201).send({
+    status: 201,
+    data: {
+      token: '',
+      id: newUser.id,
+      first_name: newUser.first_name,
+      last_name: newUser.last_name,
+      email: newUser.email,
+      is_admin: newUser.is_admin  
+    }
   });
 });
 
