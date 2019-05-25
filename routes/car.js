b7256a85ff582234fc6f1c736880a7e32b5140b5
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-25 12:05:34 +0300

    ADD: [CHORE Fixes #166253110] create car controller

--- routes/car.js
+++ routes/car.js
@@ -3,17 +3,11 @@
 /* jshint node: true */
 
 import express from 'express';
-import model from '../db/db';
+import car from '../controllers/car/car';
 
 const routerCar = express.Router();
 
 // get all cars
-routerCar.get('/api/v1/car', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'successfully retrieved cars',
-    cars: model.car,
-  });
-});
+routerCar.get('/api/v1/car', car.getCars);
 
 export default routerCar;
