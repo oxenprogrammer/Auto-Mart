250c7ed77993b9b8c9bb42b55a359a9161ae88d9
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-28 12:07:33 +0300

    ADD: [FEATURE Delivers #166282547] purchase car order

--- controllers/order/order.js
+++ controllers/order/order.js
@@ -4,6 +4,7 @@
 /* jshint node: true */
 
 import model from '../../db/db';
+import filterValue from '../../middleware/helper';
 
 class Order {
   getOrders(req, res) {
@@ -11,6 +12,53 @@ class Order {
       success: 'true',
       message: 'successfully retrieved orders',
       orders: model.order,
+    });
+  }
+
+  postCarOrder(req, res) {
+    if (!req.body.car_id || !req.body.amount) {
+      return res.status(400).send({
+        status: 400,
+        error: 'Something went wrong, Internal Server Error',
+      });
+    }
+
+    let lastOrderId = 0;
+    if (model.order.length > 0) {
+      lastOrderId = model.order[model.order.length - 1].id;
+    }
+
+    const carId = model.car.find(car => car.id === req.body.car_id);
+
+    if (!carId) {
+      return res.status(404).send({
+        status: 404,
+        error: `Car with ID ${req.body.car_id} not found`,
+      });
+    }
+
+    const carOrder = {
+      id: lastOrderId + 1,
+      buyer: req.id,
+      created_on: Date.now(),
+      car_id: req.body.car_id,
+      amount: req.body.amount,
+      status: 'pending',
+      date_modified: Date.now(),
+    };
+
+    model.order.push(carOrder);
+
+    return res.status(201).send({
+      status: 201,
+      data: {
+        id: carOrder.id,
+        buyer: carOrder.buyer,
+        orderedCar: carOrder.car_id,
+        created_on: carOrder.created_on,
+        amount: carOrder.price,
+        status: carOrder.status,
+      },
     });
   }
 }
--- routes/order.js
+++ routes/order.js
@@ -5,10 +5,14 @@
 import express from 'express';
 import order from '../controllers/order/order';
 import authentication from '../middleware/auth';
+import buyer from '../middleware/buyer';
 
 const routerOrder = express.Router();
 
 // get all orders
 routerOrder.get('/api/v1/order', authentication, order.getOrders);
+
+// post car purchase order
+routerOrder.post('/api/v1/order', [authentication, buyer], order.postCarOrder);
 
 export default routerOrder;
