c17537f4a9a1bfdebda759c86ff9f9965cd7ebfb
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-28 15:23:23 +0300

    UPDATE: [BUG Delivers #166285606] fix purchase order price patch

--- routes/order.js
+++ routes/order.js
@@ -6,6 +6,7 @@ import express from 'express';
 import order from '../controllers/order/order';
 import authentication from '../middleware/auth';
 import buyer from '../middleware/buyer';
+import seller from '../middleware/seller';
 
 const routerOrder = express.Router();
 
@@ -17,5 +18,8 @@ routerOrder.post('/api/v1/order', [authentication, buyer], order.postCarOrder);
 
 // update purchase order
 routerOrder.patch('/api/v1/order/:id/price', [authentication, buyer], order.updatePurchaseOrder);
+
+// accept reject purchase order
+routerOrder.patch('/api/v1/order/:id/status', [authentication, seller], order.acceptRejectPurchaseOrder);
 
 export default routerOrder;
