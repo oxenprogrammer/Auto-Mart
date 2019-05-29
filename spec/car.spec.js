c17ec734070b546b4c5f7a6ea14528b32b72d401
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-29 20:19:42 +0300

    ADD: [FEATURE Fixes #166328826] test get all cars require authentication

--- spec/car.spec.js
+++ spec/car.spec.js
@@ -75,8 +75,69 @@ describe('Server', () => {
             });
         });
 
-        it('should return 200', () => {
+        it('should return 200 for available cars', () => {
             expect(data.status).toBe(200);
+        });    
+    });
+
+    describe('GET /available/max_price/min_price', () => {
+        beforeAll((done) => {
+            options = {
+                url: 'http://127.0.0.1:3000/api/v1/car?status=available&min_price=2000&max_price=6000',
+                headers: {
+                    'x-auth-token': token
+                }
+            };
+            request.get(options, (error, response, body) => {
+                data.status = response.statusCode;
+                data.body = body;
+                done();
+            });
+        });
+
+        it('should return 200 if car is available within price range', () => {
+            expect(data.status).toBe(200);
+        });    
+    });
+
+    describe('GET /available/max_price/min_price', () => {
+        beforeAll((done) => {
+            options = {
+                url: 'http://127.0.0.1:3000/api/v1/car?status=available&min_price=90000&max_price=100000',
+                headers: {
+                    'x-auth-token': token
+                }
+            };
+            request.get(options, (error, response, body) => {
+                data.status = response.statusCode;
+                data.body = JSON.parse(body);
+                done();
+            });
+        });
+
+        it('should return an empty array of length 0', () => {
+            expect(data.body.data.length).toEqual(0);
+        });    
+    });
+
+    describe('GET /', () => {
+        beforeAll((done) => {
+            options = {
+                url: 'http://127.0.0.1:3000/api/v1/car',
+                headers: {
+                    'x-auth-token': 'vyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaXNfYWRtaW4iOnRydWUsInVzZXJfY2xhc3MiOiJTRUxMRVIiLCJpYXQiOjE1NTkxNDg4NTYsImV4cCI6MTU1OTIzNTI1Nn0.N6Mi4Qgk0RPcCk6DedYPxHaf42syfGtTsL8QVgeb3s8',
+                }
+            };
+            request.get(options, (error, response, body) => {
+                data.status = response.statusCode;
+                data.body = JSON.parse(body);
+                done();
+            });
+        });
+
+        it('should return an empty array of length 0', () => {
+            expect(data.body.error.message).toBe('invalid token');
+            expect(data.body.error.name).toBe('JsonWebTokenError');
         });    
     });
 });
