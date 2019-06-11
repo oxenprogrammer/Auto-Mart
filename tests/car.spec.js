0ece5ac633cf318bf21101b6800c149a152414af
oxenprogrammer <emmy1000okello@gmail.com>
2019-06-11 14:46:03 +0300

    feature(test): add get all cars && all orders [Delivers #166328826]

---
+++ tests/car.spec.js
@@ -0,0 +1,46 @@
+/* jshint esversion: 8 */
+/* jshint -W097 */
+/* jshint node: true */
+
+// Import the dependencies for testing
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import jsonwebtoken from 'jsonwebtoken';
+import server from '../server';
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+
+describe('Car Adert', () => {
+    let token;
+    before(() => {
+        const user = {
+            id: 1,
+            email: 'emmy1000okello@gmail.com',
+            first_name: 'emanuel',
+            last_name: 'okello',
+            password: '123456',
+            address: 'kampala',
+            is_admin: true,
+            user_class: 'SELLER',
+          };
+            
+        token = jsonwebtoken.sign({ id: user.id, is_admin: user.is_admin, user_class: user.user_class }, 'supertopsecret', { expiresIn: '24h' });
+    });
+    describe('GET Car Advert', () => {
+        describe('GET /', () => {
+            it('should get all cars', (done) => {
+                chai.request(server)
+                    .get('/api/v1/car')
+                    .set('x-auth-token', token)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.status.should.equal(200);
+                        res.body.data.should.be.an('array');
+                        done();
+                    });
+            });
+        });
+    });
+});
