3f7fdedfd2d1882c8d8deb53d67c7dd7a0fda5bb
oxenprogrammer <emmy1000okello@gmail.com>
2019-06-11 15:40:46 +0300

    feature(test): add test get available car ads [Delivers #166328826]

--- tests/car.spec.js
+++ tests/car.spec.js
@@ -7,6 +7,7 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import jsonwebtoken from 'jsonwebtoken';
 import server from '../server';
+import model from '../db/db';
 
 // Configure chai
 chai.use(chaiHttp);
@@ -33,6 +34,57 @@ describe('Car Adert', () => {
             it('should get all cars', (done) => {
                 chai.request(server)
                     .get('/api/v1/car')
+                    .set('x-auth-token', token)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.status.should.equal(200);
+                        res.body.data.should.be.an('array');
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe('GET Car Available Advert', () => {
+        beforeEach(() => {
+            model.car = [
+                {
+                    id: 1,
+                    owner: 1,
+                    status: 'available',
+                    state: 'new',
+                    created_on: Date.now(),
+                    price: 200.5,
+                    model: 'vs4-emmisteel',
+                    body_type: 'car',
+                    date_modified: Date.now(),
+                    
+                },
+            ];
+        });
+
+        afterEach(() => {
+            model.car = [];
+        });
+
+        describe('GET /available', () => {
+            it('should get all available cars', (done) => {
+                chai.request(server)
+                    .get('/api/v1/car?status=available')
+                    .set('x-auth-token', token)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.status.should.equal(200);
+                        res.body.data.should.be.an('array');
+                        done();
+                    });
+            });
+        });
+
+        describe('GET /available/min_price/max_price', () => {
+            it('should get all available cars with price range', (done) => {
+                chai.request(server)
+                    .get('/api/v1/car?status=available&min_price=100&max_price=300')
                     .set('x-auth-token', token)
                     .end((err, res) => {
                         res.should.have.status(200);
