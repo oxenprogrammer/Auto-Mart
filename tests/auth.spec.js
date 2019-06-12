7ce8ccc81246e1ef6a63912ba5cc44eec439ea2a
oxenprogrammer <emmy1000okello@gmail.com>
2019-06-12 16:26:11 +0300

    feature(test): add test signin user unhappy path [Fixes #166635505]

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -6,6 +6,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import jsonwebtoken from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs';
 import server from '../server';
 import model from '../db/db';
 
@@ -42,9 +43,144 @@ describe('Auth', () => {
                 .set('x-auth-token', token)
                 .end((err, res) => {
                     res.should.have.status(201);
+                    res.body.status.should.equal(201);
+                    res.body.data.token.should.be.a('string');
                     done();
                 });
         });
+
+        it('should create return 400', (done) => {
+            delete user.email;
+            chai.request(server)
+                .post('/api/v1/auth/signup')
+                .send(user)
+                .set('x-auth-token', token)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.status.should.equal(400);
+                    res.body.error.should.be.a('string');
+                    done();
+                });
+        });
+
+        describe('EMAIL must be unique', () => {
+            beforeEach((done) => {
+                model.user = [
+                    {
+                        first_name: 'emanuel',
+                        last_name: 'okello',
+                        email: 'ox@gmail.com',
+                        password: '12345',
+                        address: 'uganda',
+                        user_class: 'BUYER',
+                        is_admin: false,
+                    }
+                ];
+                done();
+            });
+
+            afterEach((done) => {
+                model.user = [];
+                done();
+            });
+
+            it('should create return 409', (done) => {
+                chai.request(server)
+                    .post('/api/v1/auth/signup')
+                    .send(user)
+                    .set('x-auth-token', token)
+                    .end((err, res) => {
+                        res.should.have.status(409);
+                        res.body.status.should.equal(409);
+                        res.body.error.should.be.a('string');
+                        done();
+                    });
+            });
+    
+
+        });
+    });
+
+    describe('USER SIGNIN', () => {
+        let user;
+        beforeEach((done) => {
+            user = {
+                    first_name: 'emanuel',
+                    last_name: 'okello',
+                    email: 'ox@gmail.com',
+                    password: '12345',
+                    address: 'uganda',
+                    user_class: 'BUYER',
+                    is_admin: false,
+                };
+
+            user = { email: 'ox@gmail.com',  password: '12345',};
+            done();
+        });
+
+        it('should return 400', (done) => {
+            chai.request(server)
+                .post('/api/v1/auth/signin')
+                .send(user)
+                .set('x-auth-token', token)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.status.should.equal(400);
+                    res.body.error.should.be.a('string');
+                    done();
+                });
+        });
+
+        // it('should create return 400', (done) => {
+        //     delete user.email;
+        //     chai.request(server)
+        //         .post('/api/v1/auth/signup')
+        //         .send(user)
+        //         .set('x-auth-token', token)
+        //         .end((err, res) => {
+        //             res.should.have.status(400);
+        //             res.body.status.should.equal(400);
+        //             res.body.error.should.be.a('string');
+        //             done();
+        //         });
+        // });
+
+        // describe('EMAIL must be unique', () => {
+        //     beforeEach((done) => {
+        //         model.user = [
+        //             {
+        //                 first_name: 'emanuel',
+        //                 last_name: 'okello',
+        //                 email: 'ox@gmail.com',
+        //                 password: '12345',
+        //                 address: 'uganda',
+        //                 user_class: 'BUYER',
+        //                 is_admin: false,
+        //             }
+        //         ];
+        //         done();
+        //     });
+
+        //     afterEach((done) => {
+        //         model.user = [];
+        //         done();
+        //     });
+
+        //     it('should create return 409', (done) => {
+        //         chai.request(server)
+        //             .post('/api/v1/auth/signup')
+        //             .send(user)
+        //             .set('x-auth-token', token)
+        //             .end((err, res) => {
+        //                 res.should.have.status(409);
+        //                 res.body.status.should.equal(409);
+        //                 res.body.error.should.be.a('string');
+        //                 done();
+        //             });
+        //     });
+    
+
+        // });
     });
 });
 
